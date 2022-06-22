5d7706272e83e119ccf7361d3e581295dd254a7d
robbyikusuma88@gmail.com <robbyikusuma88@gmail.com>
2022-06-22 08:51:12 +0700

    Runtime Memory

---
+++ src/programmer/zaman/now/classes/RuntimeApp.java
@@ -0,0 +1,14 @@
+package programmer.zaman.now.classes;
+
+public class RuntimeApp {
+    public static void main(String[] args) {
+
+        //System.gc();
+        Runtime runtime = Runtime.getRuntime();
+
+        System.out.println(runtime.availableProcessors());
+        System.out.println(runtime.freeMemory());
+        System.out.println(runtime.totalMemory());
+        System.out.println(runtime.maxMemory());
+    }
+}
